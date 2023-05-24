11d905f6309ba27a420cee39eec7573bd1052659
Joe Nace <jwnace@gmail.com>
2023-05-24 11:25:06 -0400

    initial cleanup and refactoring

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,62 +2,82 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private int _score1 = 0;
+        private int _score2 = 0;
+        private string _player1Name;
+        private string _player2Name;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            _player1Name = player1Name;
+            _player2Name = player2Name;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+            {
+                _score1++;
+            }
             else
-                m_score2 += 1;
+            {
+                _score2++;
+            }
         }
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
-            {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
+            var score = "";
 
-                }
-            }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            if (_score1 == _score2)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                // the score is tied
+                score = _score1 switch
+                {
+                    0 => "Love-All",
+                    1 => "Fifteen-All",
+                    2 => "Thirty-All",
+                    _ => "Deuce"
+                };
+            }
+            else if (_score1 >= 4 || _score2 >= 4)
+            {
+                // we MIGHT have a winner
+                var minusResult = _score1 - _score2;
+
+                if (minusResult == 1)
+                {
+                    score = "Advantage player1";
+                }
+                else if (minusResult == -1)
+                {
+                    score = "Advantage player2";
+                }
+                else if (minusResult >= 2)
+                {
+                    score = "Win for player1";
+                }
+                else
+                {
+                    score = "Win for player2";
+                }
             }
             else
             {
+                // the score is NOT tied, and nobody has won yet
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    var tempScore = 0;
+
+                    if (i == 1)
+                    {
+                        tempScore = _score1;
+                    }
+                    else
+                    {
+                        score += "-";
+                        tempScore = _score2;
+                    }
                     switch (tempScore)
                     {
                         case 0:
